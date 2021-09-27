df5ae506c0dd0944651f0528ba0fb5eb3b6f07f2
Fenroe <scrambles9494@gmail.com>
2021-09-27 15:56:40 +0100

    feat: education helper functions

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,6 +5,36 @@ class Education extends Component {
     super(props);
   }
 
+  updateInstitution(institution) {
+    const data = this.props.data;
+    data.institution = institution;
+    this.props.update(data);
+  }
+
+  updateSubject(subject) {
+    const data = this.props.data;
+    data.subject = subject;
+    this.props.update(data);
+  }
+
+  updateStartDate(startDate) {
+    const data = this.props.data;
+    data.startDate = startDate;
+    this.props.update(data);
+  }
+
+  updateEndDate(endDate) {
+    const data = this.props.date;
+    data.endDate = endDate;
+    this.props.update(data);
+  }
+
+  updateDescription(description) {
+    const data = this.props.data;
+    data.description = description;
+    this.props.update(data);
+  }
+
   renderData() {
     const renderedData = this.props.data.map(item => {
       return(
