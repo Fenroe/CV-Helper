fef9265979d5afd4022764c3596537ef31c7a18b
Fenroe <scrambles9494@gmail.com>
2021-09-27 14:41:41 +0100

    feat: education 1/3

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,9 +1,55 @@
 import React, { Component } from "react";
 
 class Education extends Component {
+  constuctor(props) {
+    super(props);
+  }
+
+  renderData() {
+    const renderedData = this.props.data.map(item => {
+      return(
+        <div
+        key = {item.key}
+        >
+          <label>Institution</label>
+          <input
+          defaultValue = {item.institution}
+          />
+          <label>Subject</label>
+          <input 
+          defaultValue = {item.subject}
+          />
+          <label>Start Date</label>
+          <input
+          defaultValue = {item.startDate}
+          />
+          <label>End Date</label>
+          <input
+          defaultValue = {item.endDate}
+          />
+          <label>Description</label>
+          <input
+          defaultValue = {item.description}
+          />
+        </div>
+      )
+    })
+    return renderedData;
+  }
+
   render() {
     return(
-      <div></div>
+      <div>
+        <h2>Education</h2>
+        <form
+        onSubmit = {e => e.preventDefault()}>
+
+          <button
+          onClick = {this.props.add}>
+            Add Institution
+          </button>
+        </form>
+      </div>
     )
   }
 }
